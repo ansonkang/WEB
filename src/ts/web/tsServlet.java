a93988db426f047e1f11301bab9f4e2240e1632c
ansonkang <kangcs.xm@gmail.com>
2015-07-31 09:32:15 +0800

    Revert "Revert "Revert "登录窗口设置，增加hibernat相关配置"""
    This reverts commit 86610a6b4676d2874ee767c1e3d6ce817f8f810d.

--- src/ts/web/tsServlet.java
+++ src/ts/web/tsServlet.java
@@ -8,48 +8,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.hibernate.Hibernate;
-import org.hibernate.Session;
-import org.hibernate.SessionBuilder;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-
-import Hibernate.HibernateSessionFactory;
-import Hibernate.user.Cus;
-import Hibernate.user.CusDAO;
-
 public class tsServlet extends HttpServlet {
 
 	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
 	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
 	 */
-	Session session=null;
-	Transaction ts=null;
-	CusDAO cusDao=new CusDAO();
-	Cus cus=new Cus();
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8");
         PrintWriter out = response.getWriter();
-        request.setCharacterEncoding("utf-8");
+        request.setCharacterEncoding("gb2312");
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        if(cusDao.findByName(username).size()!=0)
-        	{
-        	response.getWriter().write("用户名重复");
-        	}
-        else{
-	        cus.setName(username);
-	        cus.setPsWord(password);
-	        session=HibernateSessionFactory.getSession();
-	    	ts=session.beginTransaction();
-//	        ts.begin();
-	        cusDao.save(cus);
-	        ts.commit();
-	        session.close();
-	        System.out.println("保存成功");
-	        response.getWriter().write("保存成功");
-	        }
+        System.out.println(username+"|"+password);
 	}
 
 }
